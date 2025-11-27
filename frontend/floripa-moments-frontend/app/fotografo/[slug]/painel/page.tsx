498ef09f504bd6357175ed6b1250cb44f34bbacc
MatheusMBarros <matheusmbarros01@gmail.com>
2025-11-27 18:09:54 -0300

    fix painel

--- frontend/floripa-moments-frontend/app/fotografo/[slug]/painel/page.tsx
+++ frontend/floripa-moments-frontend/app/fotografo/[slug]/painel/page.tsx
@@ -116,7 +116,6 @@ export default function PhotographerPanel() {
             />
           </section>
         </div>
-        <MediaManager eventSlug={userData.event_slug} />
       </main>
       <Footer />
     </>
